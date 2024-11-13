74354daa85ecdb9872c8b93fb4f52bdd3548235f
AKASH PAL <akashpal2122003@gmail.com>
2024-11-13 21:08:11 +0530

    first

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-console.log("My name is Akash Pal");
-let n:number = 5;
-n = 8;
+console.log("My name is Akash Pal")
+let n:number = 5
+n = 8
 
 // @ts-ignore
 function sum(a:number, b:number) {
     return a + b
 }
 
-console.log(sum(10, 6));
+console.log(sum(10, 6))
