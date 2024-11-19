723538a9ef0d87eb92ad18791a4eff2e93e1e61c
AKASH PAL <akashpal2122003@gmail.com>
2024-11-19 13:28:44 +0530

    next

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 //console.log("My name is Akash Pal")
 //let n:number = 5
 //n = 8
-
 // @ts-ignore
 //function sum(a:number, b:number) {
 //  return a + b
 //}
-
 //console.log(sum(10, 6))
-
 //let isMyNameAkash:boolean = true;
 //let isDone:boolean = true;
 //let hasStarted:boolean = true;
-
 //hasStarted = "yes";
-
 /*
 function isEven(a:number):boolean {
     return a % 2 === 0;
@@ -29,6 +24,5 @@ function isDivisibleby4and8(x:number):boolean {
 console.log(isDivisibleby4and8(5));
 console.log(isDivisibleby4and8(16));
 */
-
-let bigNumber:bigint = 9007199254740992n;
+var bigNumber = 9007199254740992n;
 console.log(bigNumber);
